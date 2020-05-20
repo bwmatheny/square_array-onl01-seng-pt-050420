ab27c590404269937f2d2b99b19eb8b2169f70a9
Brian Matheny <bwmatheny@gmail.com>
2020-05-20 18:55:52 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,13 +1,3 @@
-require 'pry'
-
-
 def square_array(array)
-  array_squared = []
-  array.each {|number| array_squared << number **2}
-  array_squared
+  
 end
-
-#def collect_array(array)
-#  array.collect {|number|**2}
-#  array
-#end
